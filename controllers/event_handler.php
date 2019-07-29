2c632f4dc34e6d0a37dcbcfbd309a3b0eec828ff
rhperez <robertoh@ciencias.unam.mx>
2019-07-29 02:43:30 -0500

    Funcionalidad de registro de actividad mejorada

--- controllers/event_handler.php
+++ controllers/event_handler.php
@@ -6,47 +6,48 @@
 * @return true si y sólo si el evento se manejó exitosamente
 */
 function projectEventHandler ($json_payload) {
-  $action = $json_payload->action;
-  $action_details = null;
-  switch ($action) {
-    case "created":
-    case "closed":
-    case "reopened":
-    case "deleted":
-    break;
-    case "edited":
-    $changes = $json_payload->changes;
-    if ($changes) {
-      $action_details = "Last values:";
-      foreach ($changes as $key => $value) {
-        $action_details .= "\n".$key.": ".$value->from;
-      }
-    } else {
-      $action = "updated";
-    }
-    break;
+  if (!$json_payload || !$json_payload->project) {
+    return false;
   }
+  $module = 'projectEventHandler';
+  $action = $json_payload->action;
+
   if ($json_payload->organization) {
     // TODO upsertOrganization
     $organization_id = $json_payload->organization->id;
+    $organization = $json_payload->organization->login;
   }
+
   if ($json_payload->sender) {
     // TODO upsertSender
     $created_by = $json_payload->sender->login;
   }
+
   if ($json_payload->repository) {
+    $affected_rows = upsertRepository($json_payload->repository);
     $repository_id = $json_payload->repository->id;
-    upsertRepository($json_payload->repository);
-    insertActionLog('project', $json_payload->project->id, "linked", "repo_id: ".$repository_id, $created_by);
+    $details = "repo_id: ".$repository_id;
+    activity_log($module, $action, 'upsertRepository', transaction_result_log('upsert', $affected_rows), $details, $created_by, $organization);
   }
-  if (upsertProject($json_payload->project, $repository_id) < 0) {
+
+  $project_id = $json_payload->project->id;
+  $details = "project_id: ".$project_id;
+  if ($json_payload->changes) {
+    $details .= detail_changes_log($json_payload->changes);
+  }
+  $affected_rows = upsertProject($json_payload->project, $repository_id);
+  activity_log($module, $action, 'upsertProject', transaction_result_log('upsert', $affected_rows), $details, $created_by, $organization);
+  if ($affected_rows < 0) {
     return false;
   }
-  insertActionLog('project', $json_payload->project->id, $action, $action_details, $created_by);
-  if ($organization_id && upsertOrganizationProject($organization_id, $json_payload->project->id) < 0) {
-    return false;
+  //insertActionLog('project', $json_payload->project->id, $action, $action_details, $created_by);
+  if ($organization_id) {
+    $affected_rows = insertOrganizationProject($organization_id, $project_id);
+    $details = "organization_id: ".$organization_id;
+    $details .= "\nproject_id: ".$project_id;
+    activity_log($module, $action, 'insertOrganizationProject', transaction_result_log('insert', $affected_rows), $details, $created_by, $organization);
   }
-  insertActionLog('project', $json_payload->project->id, "linked", "organization_id: ".$organization_id, $created_by);
+
   return true;
 }
 
@@ -56,41 +57,45 @@ function projectEventHandler ($json_payload) {
 * @return true si y sólo si el evento se manejó exitosamente
 */
 function projectColumnEventHandler ($json_payload) {
-  $action = $json_payload->action;
-  $action_details = null;
-  switch ($action) {
-    case "created":
-    case "deleted":
-    break;
-    case "edited":
-    $changes = $json_payload->changes;
-    if ($changes) {
-      $action_details = "Last values:";
-      foreach ($changes as $key => $value) {
-        $action_details .= "\n".$key.": ".$value->from;
-      }
-    } else {
-      $action = "updated";
-    }
-    break;
-    case "moved":
-    if ($after_id = $json_payload->project_column->after_id) {
-      $action_details = "After_id: ".$after_id;
-    }
-    break;
+  if (!$json_payload || !$json_payload->project_column) {
+    return false;
   }
+  $module = 'projectColumnEventHandler';
+  $action = $json_payload->action;
+
   if ($json_payload->organization) {
     // TODO upsertOrganization
     $organization_id = $json_payload->organization->id;
+    $organization = $json_payload->organization->login;
   }
+
   if ($json_payload->sender) {
     // TODO upsertSender
     $created_by = $json_payload->sender->login;
   }
-  if (upsertProjectColumn($json_payload->project_column) < 0) {
+
+  if ($json_payload->repository) {
+    $affected_rows = upsertRepository($json_payload->repository);
+    $repository_id = $json_payload->repository->id;
+    $details = "repo_id: ".$repository_id;
+    activity_log($module, $action, 'upsertRepository', transaction_result_log('upsert', $affected_rows), $details, $created_by, $organization);
+  }
+
+  $column_id = $json_payload->project_column->id;
+  $details = "column_id: ".$column_id;
+  if ($json_payload->changes) {
+    $details .= detail_changes_log($json_payload->changes);
+  }
+  if ($after_id = $json_payload->project_column->after_id) {
+    $details .= "\nafter_id: ".$after_id;
+  }
+
+  $affected_rows = upsertProjectColumn($json_payload->project_column);
+  activity_log($module, $action, 'upsertProjectColumn', transaction_result_log('upsert', $affected_rows), $details, $created_by, $organization);
+  if ($affected_rows < 0) {
     return false;
   }
-  insertActionLog('project_column', $json_payload->project_column->id, $action, $action_details, $created_by);
+
   return true;
 }
 
@@ -100,37 +105,42 @@ function projectColumnEventHandler ($json_payload) {
 * @return true si y sólo si el evento se manejó exitosamente
 */
 function projectCardEventHandler ($json_payload) {
-  $action = $json_payload->action;
-  $action_details = null;
-  switch ($action) {
-    case "created":
-    case "deleted":
-    break;
-    case "edited":
-    case "moved":
-    case "converted":
-    $changes = $json_payload->changes;
-    if ($changes) {
-      $action_details = "Last values:";
-      foreach ($changes as $key => $value) {
-        $action_details .= "\n".$key.": ".$value->from;
-      }
-    } else {
-      $action = "updated";
-    }
+  if (!$json_payload || !$json_payload->project_card) {
+    return false;
   }
+  $module = 'projectCardEventHandler';
+  $action = $json_payload->action;
+
   if ($json_payload->organization) {
     // TODO upsertOrganization
     $organization_id = $json_payload->organization->id;
+    $organization = $json_payload->organization->login;
   }
+
   if ($json_payload->sender) {
     // TODO upsertSender
     $created_by = $json_payload->sender->login;
   }
-  if (upsertProjectCard($json_payload->project_card) < 0) {
+
+  if ($json_payload->repository) {
+    $affected_rows = upsertRepository($json_payload->repository);
+    $repository_id = $json_payload->repository->id;
+    $details = "repo_id: ".$repository_id;
+    activity_log($module, $action, 'upsertRepository', transaction_result_log('upsert', $affected_rows), $details, $created_by, $organization);
+  }
+
+  $card_id = $json_payload->project_card->id;
+  $details = "card_id: ".$card_id;
+  if ($json_payload->changes) {
+    $details .= detail_changes_log($json_payload->changes);
+  }
+
+  $affected_rows = upsertProjectCard($json_payload->project_card);
+  activity_log($module, $action, 'upsertProjectCard', transaction_result_log('upsert', $affected_rows), $details, $created_by, $organization);
+  if ($affected_rows < 0) {
     return false;
   }
-  insertActionLog('project_card', $json_payload->project_card->id, $action, $action_details, $created_by);
+
   return true;
 }
 
@@ -140,8 +150,12 @@ function projectCardEventHandler ($json_payload) {
 * @return true si y sólo si el evento se manejó exitosamente
 */
 function issuesEventHandler ($json_payload) {
+  if (!$json_payload || !$json_payload->issue) {
+    return false;
+  }
+  $module = 'issuesEventHandler';
   $action = $json_payload->action;
-  $action_details = null;
+
   switch ($action) {
     case "opened":
     case "closed":
@@ -172,34 +186,57 @@ function issuesEventHandler ($json_payload) {
       }
 
   }
+
   if ($json_payload->organization) {
     // TODO upsertOrganization
     $organization_id = $json_payload->organization->id;
+    $organization = $json_payload->organization->login;
   }
+
   if ($json_payload->sender) {
     // TODO upsertSender
     $created_by = $json_payload->sender->login;
   }
+
   if ($json_payload->repository) {
-    upsertRepository($json_payload->repository);
+    $affected_rows = upsertRepository($json_payload->repository);
     $repository_id = $json_payload->repository->id;
-    insertActionLog('issue', $json_payload->issue->id, "linked", "repo_id: ".$repository_id, $created_by);
+    $details = "repo_id: ".$repository_id;
+    activity_log($module, $action, 'upsertRepository', transaction_result_log('upsert', $affected_rows), $details, $created_by, $organization);
   }
+
   if ($json_payload->issue->milestone) {
-    upsertMilestone($json_payload->issue->milestone, $repository_id);
+    $affected_rows = upsertMilestone($json_payload->issue->milestone, $repository_id);
     $milestone_id = $json_payload->issue->milestone->id;
-    insertActionLog('issue', $json_payload->issue->id, "linked", "milestone_id: ".$milestone_id, $created_by);
+    $details = "milestone_id: ".$milestone_id;
+    activity_log($module, $action, 'upsertMilestone', transaction_result_log('upsert', $affected_rows), $details, $created_by, $organization);
   }
-  unlabelIssue($json_payload->issue->id);
-  foreach ($json_payload->issue->labels as $json_label) {
-    upsertLabel($json_label, $repository_id);
-    upsertIssueLabel($json_payload->issue->id, $json_label->id);
-    insertActionLog('issue', $json_payload->issue->id, "linked", "label_id: ".$json_label->id, $created_by);
+
+  $issue_id = $json_payload->issue->id;
+  $details = "issue_id: ".$issue_id;
+
+  $affected_rows = unlabelIssue($issue_id);
+  activity_log($module, $action, 'unlabelIssue', transaction_result_log('update', $affected_rows), $details, $created_by, $organization);
+
+  if ($json_payload->changes) {
+    $details .= detail_changes_log($json_payload->changes);
   }
-  if (upsertIssue($json_payload->issue, $repository_id, $milestone_id) < 0) {
+  $affected_rows = upsertIssue($json_payload->issue, $repository_id, $milestone_id);
+  activity_log($module, $action, 'upsertIssue', transaction_result_log('upsert', $affected_rows), $details, $created_by, $organization);
+  if ($affected_rows < 0) {
     return false;
   }
-  insertActionLog('issue', $json_payload->issue->id, $action, $action_details, $created_by);
+
+  foreach ($json_payload->issue->labels as $json_label) {
+    $affected_rows = upsertLabel($json_label, $repository_id);
+    $details = "label_id: ".$json_label->id;
+    activity_log($module, $action, 'upsertLabel', transaction_result_log('upsert', $affected_rows), $details, $created_by, $organization);
+    $affected_rows = upsertIssueLabel($issue_id, $json_label->id);
+    $details = "issue_id: ".$issue_id;
+    $details .= "\nlabel_id: ".$json_label->id;
+    activity_log($module, $action, 'upsertIssueLabel', transaction_result_log('upsert', $affected_rows), $details, $created_by, $organization);
+  }
+
   return true;
 }
  ?>
